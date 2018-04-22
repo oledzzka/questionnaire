ce07d7cbaba1a7a9d87b5c86ec72a4bb9e4b2614
Buslovsky Oleg <oledzzka@mail.ru>
2018-04-22 16:12:58 +0300

    core/admin, static, requirments: Добавлено отображение в админке моделей core/models

--- questionnaire/core/admin.py
+++ questionnaire/core/admin.py
@@ -1,3 +1,28 @@
+import nested_admin
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Questionnaire, Question, UserAnswer, AnswersList
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    pass
+
+
+class ToAnswerListInline(nested_admin.NestedTabularInline):
+    model = AnswersList
+    extra = 0
+    sortable_field_name = 'my_order'
+
+
+class ToQuestionInline(nested_admin.NestedTabularInline):
+    model = Question
+    extra = 0
+    inlines = [ToAnswerListInline]
+    sortable_field_name = 'my_order'
+
+
+@admin.register(Questionnaire)
+class TableQuestionnaire(nested_admin.NestedModelAdmin):
+    inlines = [ToQuestionInline]
+
+admin.site.register(UserAnswer)
+admin.site.register(User)
